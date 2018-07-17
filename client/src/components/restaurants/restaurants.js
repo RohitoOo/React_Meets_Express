6090ed8671e17b352734253d99d39c5047a9d079
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-07-17 19:59:44 +0530

    Beautified Code, Added Bootstrap CDN to client Html page And Passed Image as Json Object from Express to React ( BackEnd to FrontEnd )

--- client/src/components/restaurants/restaurants.js
+++ client/src/components/restaurants/restaurants.js
@@ -1,45 +1,47 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './restaurants.css';
+import { Button , Jumbotron } from 'react-bootstrap';
 
 class Restaurants extends Component {
 
-constructor(){
-//super() - As this is being embeded into a parent Component
-  super();
-  this.state = {
-    restaurants : []
+  constructor() {
+    //super() - As this is being embeded into a parent Component
+    super();
+    this.state = {
+      restaurants: []
+    }
   }
-}
-//Used Proxy in Package.json
-//fetch(http://localhost:5000/restaurants)
+  //Used Proxy in Package.json
+  //fetch(http://localhost:5000/restaurants)
 
-componentDidMount(){
+  componentDidMount() {
 
-   fetch('/restaurants')
-  .then(res => res.json())
-  .then(restaurants => this.setState({restaurants}, () =>
-    console.log("Restaurants are being fetched from Express" , restaurants)
-  ))
-}
+    fetch('/restaurants').then(res => res.json()).then(restaurants => this.setState({
+      restaurants
+    }, () => console.log("Restaurants are being fetched from Express", restaurants)))
+  }
 
   render() {
-    return (
-      <div >
-        <h1>List Of Available Restaurants</h1>
-        <ul>
-      {this.state.restaurants.map( (restaurant) => {
-        return   <li>  <img src={require('./dish3.jpg')} />
+    return (<div >
+      <h1>List Of Available Restaurants</h1>
 
-          <img src={require('./dish4.jpg')} />
+      <ul>
 
- {restaurant.restaurantId} <br/> {restaurant.restaurantName} <br/> {restaurant.restaurantPhoneNumber}
-<img  src={require('./dish1.jpg')} />
-<img src={require('./dish2.jpg')} />
- </li>
-        }) }
+        {
+          this.state.restaurants.map((restaurant) => {
+            return <Jumbotron >
+              <li>
+              <img src={restaurant.restaurantImage}/>
+              <br/> &nbsp; {restaurant.restaurantName} &nbsp;
+                {restaurant.restaurantPhoneNumber}
+            </li>
+            </Jumbotron>
+          })
+        }
+
       </ul>
-      </div>
-    );
+
+    </div>);
   }
 }
 
--- server.js
+++ server.js
@@ -2,70 +2,57 @@ const express = require('express')
 
 const app = express();
 
-app.get('/restaurants',  (req,res) => {
+app.get('/restaurants', (req, res) => {
 
-
-// This info will be pull from the mongo database using mongoose query
-  const restaurants = [ {
-	"restaurantId" : "1.",
-	"restaurantName" : "Burger King",
-	"restaurantPhoneNumber" : "998923976",
-  "restaurantImage" : [
+  // This info will be pull from the mongo database using mongoose query
+  const restaurants = [
     {
-     "url": "images/dish1.jpg"
-  }]
+      "restaurantId": "1.",
+      "restaurantName": "Burger King",
+      "restaurantPhoneNumber": "998923976",
+      "restaurantImage": "https://image.ibb.co/mFYRKJ/burger_King.png" 
 
-}, {
-  "restaurantId" : "2.",
-	"restaurantName" : "KFC",
-	"restaurantPhoneNumber" : "998923976",
+    }, {
+      "restaurantId": "2.",
+      "restaurantName": "KFC",
+      "restaurantPhoneNumber": "998923976"
+    }, {
+      "restaurantId": "3.",
+      "restaurantName": "McDonalds",
+      "restaurantPhoneNumber": "998923976"
+    }, {
+      "restaurantId": "4.",
+      "restaurantName": "Burger King",
+      "restaurantPhoneNumber": "998923976"
 
-},
-{
-  "restaurantId" : "3.",
-	"restaurantName" : "McDonalds",
-	"restaurantPhoneNumber" : "998923976",
-},
-{
-"restaurantId" : "4.",
-"restaurantName" : "Burger King",
-"restaurantPhoneNumber" : "998923976"
+    }, {
+      "restaurantId": "5.",
+      "restaurantName": "KFC",
+      "restaurantPhoneNumber": "998923976"
+    }, {
+      "restaurantId": "6.",
+      "restaurantName": "McDonalds",
+      "restaurantPhoneNumber": "998923976"
+    }, {
+      "restaurantId": "7.",
+      "restaurantName": "Burger King",
+      "restaurantPhoneNumber": "998923976"
 
-}, {
-"restaurantId" : "5.",
-"restaurantName" : "KFC",
-"restaurantPhoneNumber" : "998923976",
-
-},
-{
-"restaurantId" : "6.",
-"restaurantName" : "McDonalds",
-"restaurantPhoneNumber" : "998923976",
-},
-{
-"restaurantId" : "7.",
-"restaurantName" : "Burger King",
-"restaurantPhoneNumber" : "998923976"
-
-}, {
-"restaurantId" : "8.",
-"restaurantName" : "KFC",
-"restaurantPhoneNumber" : "998923976",
-
-},
-{
-"restaurantId" : "9.",
-"restaurantName" : "McDonalds",
-"restaurantPhoneNumber" : "998923976",
-},
-
+    }, {
+      "restaurantId": "8.",
+      "restaurantName": "KFC",
+      "restaurantPhoneNumber": "998923976"
+    }, {
+      "restaurantId": "9.",
+      "restaurantName": "McDonalds",
+      "restaurantPhoneNumber": "998923976"
+    }
   ]
 
-res.json(restaurants)
+  res.json(restaurants)
 
 })
 
-
-app.listen(5000, (req, res) =>{
+app.listen(5000, (req, res) => {
   console.log("Express Server Running on Port 5000")
 })
